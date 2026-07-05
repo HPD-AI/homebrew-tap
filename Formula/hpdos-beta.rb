d995a183c519319480df76269c42c6a5198c5559
Ewoofcoding <einsteinessibu26@outlook.com>
2026-07-04 21:30:41 -0500

    Point HPDOS beta formula at public Homebrew tap release assets

--- Formula/hpdos-beta.rb
+++ Formula/hpdos-beta.rb
@@ -7,20 +7,20 @@ class HpdosBeta < Formula
   if OS.mac?
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/HPD-AI/HPD-OS/releases/download/hpdos-tui-v0.1.0-beta.1/hpdos-osx-arm64.tar.gz"
+        url "https://github.com/HPD-AI/homebrew-tap/releases/download/hpdos-v0.1.0-beta.1/hpdos-osx-arm64.tar.gz"
         sha256 "298fcd34290496cbaec536f6bbde6bd29b8ea704f8eed5411b88428a8cc3a531"
       else
-        url "https://github.com/HPD-AI/HPD-OS/releases/download/hpdos-tui-v0.1.0-beta.1/hpdos-osx-x64.tar.gz"
+        url "https://github.com/HPD-AI/homebrew-tap/releases/download/hpdos-v0.1.0-beta.1/hpdos-osx-x64.tar.gz"
         sha256 "5d1067fc982bff0c6015b2a1a00899c63b574db0a923a13c4506ba8dcc5f79f7"
       end
     end
   else
     on_linux do
       if Hardware::CPU.arm?
-        url "https://github.com/HPD-AI/HPD-OS/releases/download/hpdos-tui-v0.1.0-beta.1/hpdos-linux-arm64.tar.gz"
+        url "https://github.com/HPD-AI/homebrew-tap/releases/download/hpdos-v0.1.0-beta.1/hpdos-linux-arm64.tar.gz"
         sha256 "f1d92da27810d5d17ae894b67d56c6e47c4b9d3f37a408ced0b36f4d5b5aa078"
       else
-        url "https://github.com/HPD-AI/HPD-OS/releases/download/hpdos-tui-v0.1.0-beta.1/hpdos-linux-x64.tar.gz"
+        url "https://github.com/HPD-AI/homebrew-tap/releases/download/hpdos-v0.1.0-beta.1/hpdos-linux-x64.tar.gz"
         sha256 "174c5f38ac7fc22cccb6f1f6777fca70cb09f447f1062585395bd2a09e683061"
       end
     end
